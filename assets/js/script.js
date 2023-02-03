bf8878716b9c261a9747dfa360472a1ae6e3cc7d
EN QUE TU TA WEB LEONYS <leonysgang@gmail.com>
2023-02-03 11:00:17 -0400

    EN QUE TU TA WEB ACTUALIZADO

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+const option1 = document.getElementById('option1')
+const option2 = document.getElementById('option2')
+const content1 = document.getElementById('content1')
+const content2 = document.getElementById('content2')
+
+let chose = 1
+
+const changeOption = ()=>{
+    chose==1 ?(
+        option1.classList.value='option option-active',
+        content1.classList.value='content content-active'
+    )
+    : (
+
+        option1.classList.value = 'option',
+        content1.classList.value= 'content'
+
+    )
+
+    chose== 2 ?  (
+
+        option2.classList.value='option option-active',
+        content2.classList.value='content content-active'
+
+    )
+    : (
+
+        option2.classList.value = 'option',
+        content2.classList.value= 'content'
+
+    )
+
+
+
+}
+
+option1.addEventListener('click', ()=>{
+    chose = 1
+    changeOption()
+})
+
+option2.addEventListener('click', ()=>{
+    chose = 2
+    changeOption()
+})
+//  toggle like a one class function 
+
+
+
+toggle_switch.onchange=function(){
+
+    if (this.checked) {
+        // si el checkbox está marcado, agregar clase "nueva-clase"
+        miImagen.classList.add("toggle_card_img");
+      } else {
+        // si el checkbox no está marcado, remover clase "nueva-clase"
+        miImagen.classList.remove("toggle_card_img");
+      }
+}
+
